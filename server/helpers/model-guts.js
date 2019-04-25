1eea081cf9a9073452defaf125e864c340360140
raineer <delaritaraineer81@gmail.com>
2019-04-25 13:29:15 +0800

    model guts JS " "

---
+++ server/helpers/model-guts.js
@@ -0,0 +1,62 @@
+"use strict";
+
+// The guts of a model that uses Knexjs to store and retrieve data from a
+// database using the provided `knex` instance. Custom functionality can be
+// composed on top of this set of common guts.
+
+// The idea is that these are the most-used types of functions that most/all
+// "models" will want to have. They can be overriden/modified/extended if
+// needed by composing a new object out of the one returned by this function ;)
+
+module.exports = ({
+  knex = {},
+  name = name,
+  tableName = "tablename",
+  selectableProps = [],
+  timeout = 1000
+}) => {
+  const create = props =>
+    knex
+      .insert(props)
+      .into(tableName)
+      .timeout(1000);
+
+  const findAll = () =>
+    knex
+      .select()
+      .from(tableName)
+      .timeout(1000);
+
+  const findById = id =>
+    knex
+      .select(selectableProps)
+      .from(tableName)
+      .where({ id })
+      .timeout(1000);
+
+  const update = props =>
+    knex
+      .update(props)
+      .from(tableName)
+      .where({ id: props.id })
+      .timeout(1000);
+
+  const destroy = id =>
+    knex
+      .del()
+      .from(tableName)
+      .where({ id })
+      .timeout(1000);
+
+  return {
+    name,
+    tableName,
+    selectableProps,
+    timeout,
+    create,
+    findAll,
+    findById,
+    update,
+    destroy
+  };
+};
--- server/models/content.js
+++ server/models/content.js
@@ -1,51 +1,12 @@
 "use strict";
 
+const createGuts = require("../helpers/model-guts");
+
 const name = "Postcontent";
 const tableName = "postcontent";
 
 const selectableProps = ["title", "content"];
 
 module.exports = knex => {
-  const create = props =>
-    knex
-      .insert(props)
-      .into(tableName)
-      .timeout(1000);
-
-  const findAll = () =>
-    knex
-      .select()
-      .from(tableName)
-      .timeout(1000);
-
-  const findById = id =>
-    knex
-      .select(selectableProps)
-      .from(tableName)
-      .where({ id })
-      .timeout(1000);
-
-  const update = props =>
-    knex
-      .update(props)
-      .from(tableName)
-      .where({ id: props.id })
-      .timeout(1000);
-
-  const destroy = id =>
-    knex
-      .del()
-      .from(tableName)
-      .where({ id })
-      .timeout(1000);
-
-  return {
-    name,
-    tableName,
-    create,
-    findAll,
-    findById,
-    update,
-    destroy
-  };
+  const guts = createGuts([knex, name, tableName, selectableProps]);
 };
